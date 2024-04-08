9cd579a075df5a805ea5f296c91b9a0052e0aaf0
minotte <n.minotte@gmail.com>
2024-04-08 03:51:18 -0400

    go to school

--- srcs/requierements/mariadb/conf/mysql.sh
+++ srcs/requierements/mariadb/conf/mysql.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 service mysql start;
-mysql -e " CREATE DATABASE IF NOT EXIST \`${SQL_DATABASE}\`;"
-mysql -e"CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
+
--- srcs/requierements/wordpress/conf/auto_config.sh
+++ srcs/requierements/wordpress/conf/auto_config.sh
@@ -1,5 +1,16 @@
-!/bin/sh
+#!/bin/sh
 
 sleep 10
 
-wp config create
+wp config create	--allow-root \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD \
+	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+# Installer WordPress
+wp core install --allow-root \
+    --url="http://nminotte.42.com" \
+    --title="My wonderful Site" \
+    --admin_user="nminotte" \
+    --admin_password="mySuperpwd" \
