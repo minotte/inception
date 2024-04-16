2e6440fe5fd73fc5769802049bbd9f04e9359a9d
minotte <n.minotte@gmail.com>
2024-04-16 09:50:04 -0400

    looks working need make test and Makefile

--- srcs/requierements/mariadb/conf/mysql.sh
+++ srcs/requierements/mariadb/conf/mysql.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+#chmod 644 /var/lib/mysql
+chmod 644 /etc/mysql/mariadb.conf.d/50-server.cnf ;
 service mariadb start;
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+sleep 5;
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+#mariadb -e "USE \`${MYSQL_DATABASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mariadb -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# mariadb -e "DROP TABLE IF EXISTS mariadb_users;"
+# mariadb -e "DROP TABLE IF EXISTS mariadb_posts;"
 mariadb -e "FLUSH PRIVILEGES;"
-mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mariadbd-safe
+mariadb-admin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+#docker exec mariadbd mariadbd-safe
+mysqld
--- srcs/requierements/wordpress/conf/auto_config.sh
+++ srcs/requierements/wordpress/conf/auto_config.sh
@@ -4,17 +4,24 @@ sleep 10
 
 
 
-./wp-cli.phar config create	--allow-root \
-	--dbname=$SQL_DATABASE \
-	--dbuser=$SQL_USER \
-	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    ./wp-cli.phar core download  --allow-root 
 
-# Installer WordPress
-./wp-cli.phar core install --allow-root \
-    --url="https://nminotte.42.com" \
-    --title="My wonderful Site" \
-    --admin_user="nminotte" \
-    --admin_password="mySuperpwd" \
-    --locale="fr_FR" \
-    --path='/var/www/wordpress'
+    ./wp-cli.phar config create	--allow-root \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD \
+        --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+    # Installer WordPress
+    ./wp-cli.phar core install --allow-root \
+        --url="https://nminotte.42.fr" \
+        --title="My wonderful Site" \
+        --admin_user="nminotte" \
+        --admin_password="cestunSuperpwd!" \
+        --locale="fr_FR" \
+        --admin_email="nminotte@42angouleme.com" \
+        --path='/var/www/wordpress'
+fi
+
+/usr/sbin/php-fpm81 -F
